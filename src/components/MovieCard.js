4bdea1f56249784236e315e151d2f9624085f489
yunjung0 <pyjlily@naver.com>
2025-07-14 23:00:32 +0900

    eight

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,7 +5,7 @@ export default function MovieCard({ movie }) {
   return (
     <Link to={`/movie/${movie.id}`} className="movie-card">
       <img src={movie.poster_url} alt={movie.title_kor} className='movie-poster'/>
-      <p>{movie.title_kor}</p>
+      <p className="movie-title">{movie.title_kor}</p>
     </Link>
   );
 }

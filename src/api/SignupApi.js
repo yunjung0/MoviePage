a4c2562d9e59e9f457342e1588b1d877c47c32ef
sun-ji054 <[seonji04203645@gmail.com]>
2025-07-15 04:27:35 +0900

    api 명세서 기반 새롭게 수정 하지만 회원가입, 로그인 필드 email포함 수정 필요

--- src/api/SignupApi.js
+++ src/api/SignupApi.js
@@ -3,7 +3,7 @@ import api from './Api';
 //회원가입
 export const signup = async (username, password, password2, nickname) => {
   try {
-        const res = await api.post('/users/signup/', {
+        const res = await api.post('/auth/registration/', {
             username,
             password,
             password2,
@@ -18,7 +18,7 @@ export const signup = async (username, password, password2, nickname) => {
 //로그인
 export const login = async (username, password) => {
     try {
-        const res = await api.post('/users/api/token/', {
+        const res = await api.post('/auth/login/', {
             username,
             password,
         });
@@ -32,13 +32,8 @@ export const login = async (username, password) => {
 };
 
 // 로그아웃
-export const logout = async () => {
-    try {
-        await api.post('/users/api/token/');
-        localStorage.removeItem('token');
-    } catch (err) {
-        localStorage.removeItem('token');
-    }
+export const logout = () => {
+    localStorage.removeItem('token');
 };
 
 // 현재 로그인 상태 확인
@@ -46,15 +41,15 @@ export const isAuthenticated = () => {
     return !!localStorage.getItem('token');
 };
 
-// 프로필 조회
-export const getUserProfile = async (userId) => {
-  const res = await api.get(`/users/${userId}/`);
+// 프로필 조회 (현재 로그인된 사용자)
+export const getUserProfile = async () => {
+  const res = await api.get('/users/profile/');
   return res.data;
 };
 
-// 닉네임 수정
-export const updateUserNickname = async (userId, nickname) => {
-  const res = await api.put(`/users/${userId}/`, { nickname });
+// 닉네임 수정 (현재 로그인된 사용자)
+export const updateUserNickname = async (nickname) => {
+  const res = await api.put('/users/profile/', { nickname });
   return res.data;
 };
 
